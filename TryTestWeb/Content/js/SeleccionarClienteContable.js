aaa7b8e44ebad84fc3ae95cad6fc5f78206abf77
kimi01s <ksotelocalderon@gmail.com>
2021-06-07 19:30:47 -0400

    modificar html de Seleccionar emosir desde modulos y seleccionar cliente contable dejando css y js en hojas aparte para mantener orden y agregando íconos para una mejor estética.

---
+++ TryTestWeb/Content/js/SeleccionarClienteContable.js
@@ -0,0 +1,14 @@
+﻿function SelecCliente(idClienteContable) {
+    $.ajax({
+        url: "DoSeleccionarClienteContable?EmisorID=" + idClienteContable,
+        success: function (response) {
+            if (response != null && response.success) {
+                //alert(document.location.href);
+                document.location.href = "/Contabilidad/PanelClienteContable";
+            }
+            else {
+                alert("No se puede operar con este cliente contable");
+            }
+        }
+    });
+}
---
+++ TryTestWeb/Content/js/SeleccionarEmisorDesdeModulos.js
@@ -0,0 +1,14 @@
+﻿function SelecCliente(idEmisor) {
+    $.ajax({
+        url: "DoSeleccionarEmisorModulos?EmisorID=" + idEmisor,
+        success: function (response) {
+            if (response != null && response.success) {
+                // alert(response.inicio);
+                document.location.href = "/" + response.inicio;//"/Home/Index";
+            }
+            else {
+                alert("No se puede operar con este emisor");
+            }
+        }
+    });
+}
